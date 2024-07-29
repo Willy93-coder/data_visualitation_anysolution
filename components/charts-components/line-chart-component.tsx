75965f9142e73d17f3f3edb3679eb5c157a06187
gcifre <gcifre@anysolution.org>
2024-07-29 12:02:43 +0200

    Added: line chart

--- components/charts-components/line-chart-component.tsx
+++ components/charts-components/line-chart-component.tsx
@@ -8,13 +8,20 @@ const data = [
   { name: "Page F", uv: 190, pv: 2400, amt: 2400 },
 ];
 
-export function LineChartComponent() {
+type propsChart = {
+  width: number;
+  height: number;
+};
+
+export function LineChartComponent({ width, height }: propsChart) {
   return (
-    <LineChart width={400} height={400} data={data}>
-      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-      <CartesianGrid stroke="#ccc" />
-      <XAxis dataKey="name" />
-      <YAxis />
-    </LineChart>
+    <div className="grow">
+      <LineChart width={width} height={height} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+      </LineChart>
+    </div>
   );
 }

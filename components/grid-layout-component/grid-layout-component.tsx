0953766521360b91b8f75c54b0063093af0a23da
gcifre <gcifre@anysolution.org>
2024-08-27 16:08:20 +0200

    Modified: grid layout component

--- components/grid-layout-component/grid-layout-component.tsx
+++ components/grid-layout-component/grid-layout-component.tsx
@@ -5,21 +5,16 @@ import "react-resizable/css/styles.css";
 import { WidthProvider } from "react-grid-layout";
 import { ChartContainer } from "../charts-components/container-chart";
 import { data, productSales, salesData } from "@/mockdata";
+import { ChartConfigType, ChartType } from "@/lib/utils";
 
 const ResponsiveGridLayout = WidthProvider(GridLayout);
 
-export function GridLayoutComponent() {
+export function GridLayoutComponent({ chart }: { chart?: ChartConfigType[] }) {
   const layout = [
     { i: "0", x: 0, y: 0, w: 3, h: 2 },
     { i: "1", x: 3, y: 0, w: 4, h: 2 },
     { i: "2", x: 0, y: 0, w: 4, h: 2 },
     { i: "3", x: 8, y: 0, w: 5, h: 4 },
-  ];
-  const layout2 = [
-    { i: "0", x: 0, y: 0, w: 3, h: 2 },
-    { i: "1", x: 3, y: 0, w: 4, h: 2 },
-    { i: "2", x: 0, y: 0, w: 4, h: 2 },
-    { i: "3", x: 4, y: 0, w: 5, h: 2 },
   ];
 
   return (
@@ -30,29 +25,42 @@ export function GridLayoutComponent() {
       compactType={null}
       margin={[10, 10]}
     >
+      {/* TODO: map chart */}
+      {chart?.map((el) => {
+        return (
+          <div
+            key={el.id}
+            className="bg-white flex justify-center items-center pr-4 py-4 border border-black"
+            data-grid={{ x: 0, y: 0, w: 3, h: 2 }}
+          >
+            <ChartContainer type={el.type} data={salesData} />
+          </div>
+        );
+      })}
+
       <div
         key={0}
         className="bg-white flex justify-center items-center pr-4 py-4 border border-black"
       >
-        <ChartContainer type={"line"} data={data} />
+        <ChartContainer type={ChartType.LINE} data={data} />
       </div>
       <div
         key={1}
         className="bg-white flex justify-center items-center pr-4 py-4 border border-black"
       >
-        <ChartContainer type={"area"} data={productSales} />
+        <ChartContainer type={ChartType.AREA} data={productSales} />
       </div>
       <div
         key={2}
         className="bg-white flex justify-center items-center pr-4 py-4 border border-black"
       >
-        <ChartContainer type={"bar"} data={salesData} />
+        <ChartContainer type={ChartType.BAR} data={salesData} />
       </div>
       <div
         key={3}
         className="bg-white flex justify-center items-center pr-4 py-4 border border-black"
       >
-        <ChartContainer type={"pie"} data={salesData} />
+        <ChartContainer type={ChartType.PIE} data={salesData} />
       </div>
     </ResponsiveGridLayout>
   );

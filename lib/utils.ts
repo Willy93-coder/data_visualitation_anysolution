34dffdb2a9551eea1f3f9e7999de8acc0bd4283a
gcifre <gcifre@anysolution.org>
2024-08-27 16:05:39 +0200

    Added: enum chart type, chart config schema, chart config type, chart config array and addChart method

--- lib/utils.ts
+++ lib/utils.ts
@@ -28,11 +28,29 @@ export type NgsildData = z.infer<typeof ngsiLdSchema>;
 
 export const CREATE_CHART = "Create chart";
 
+export enum ChartType {
+  AREA,
+  BAR,
+  LINE,
+  PIE,
+}
+
 export const selectOptionsCreateChartForm = [
-  { value: "area", label: "Area chart" },
-  { value: "bar", label: "Bar chart" },
-  { value: "line", label: "Line chart" },
-  { value: "pie", label: "Pie chart" },
+  { value: ChartType.AREA, label: "Area chart" },
+  { value: ChartType.BAR, label: "Bar chart" },
+  { value: ChartType.LINE, label: "Line chart" },
+  { value: ChartType.PIE, label: "Pie chart" },
 ];
 
-export const chartConfig = [];
+export const chartConfigSchema = z.object({
+  id: z.string().uuid(),
+  type: z.nativeEnum(ChartType),
+});
+
+export type ChartConfigType = z.infer<typeof chartConfigSchema>;
+
+export const chartConfig: ChartConfigType[] = [];
+
+export function addChart(chart: ChartConfigType) {
+  chartConfig.push(chart);
+}
